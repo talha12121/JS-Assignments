9f5cd6d8b184e9dbef8d246bff4b8dd113a6f2b3
Talha Khan <tk411635@gmail.com>
2022-05-18 01:11:51 +0500

    3 chapters completed

---
+++ app.js
@@ -0,0 +1,122 @@
+// Chapter no 1
+
+// question no 1
+// alert("Hello Talha");
+
+// question no 2
+// alert("Error! Please Enter A Valid Password");
+
+// question no 3
+// alert("Welcome To JS land \n Happy Coding!");
+
+// question no 4
+// alert("Welcome To JS land");
+// alert("Happy Coding!");
+
+// question no 5
+// alert("Hello... I can run JS through my web browser's console");
+
+
+// Chapter no 2
+
+// question no 1
+// var  useranme
+
+// question no 2
+// var myname = "Muhammad Talha"
+
+// question no 3
+
+// var message = "Hello world"
+// alert(message);
+
+// question no 4
+
+// var name = "Jhone Doe"
+// alert(name);
+
+// var age = "15 years old"
+// alert(age);
+
+// var certificate = "Certified Mobile Application Development"
+// alert(certificate);
+
+// question no 5
+
+// var PIZZA = "PIZZA"
+// alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
+
+
+// question no 6
+
+// var email = " tk411635@gmail.com"
+// alert("My email is" +  email); 
+
+// question no 7
+
+// var book = " A smarter way to learn JavaScript"
+// alert("I am trying to learn the book" + book);
+
+// question no 8
+// document.write("yah! i can write HTML through JS");
+// question no 9
+// var input = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬"
+// alert(input);
+
+
+
+// Chapter no 3
+
+// question No 1
+
+// var age = 19 
+// alert("I Am " + age + " Years Old");
+
+// question No 2
+
+// var visit = 14
+// alert("You Have Visited The Site " + visit + " Times");
+
+// question No 3
+
+// var birthYear = 2002
+// document.write("My Birth Year Is " + birthYear);
+
+// question No 4
+
+// var visitorsName = "John Doe"
+// var quantity = 5  
+// var productTitle = "XYZ Clothing Store"
+// document.write(visitorsName + " ordered " + quantity + " T-Shirts on " +  productTitle);
+
+
+// Chapter No 4
+
+// question no 1
+
+// var a , b , c = 1 , 3 , 2
+
+// question no 2
+
+// Legal Variable:
+// var myName 
+// var userResponseTime 
+// var my_name
+// var variable 
+// var Dollar$ign
+
+// Illegal Variable
+// var my name 
+// var 123 
+// var You&I
+// var http:
+// var ^my_variable
+
+// question no 3
+
+document.write("<h1>" + "Rules For Naming JS Variables" + "</h1>" );
+
+
+
+
+
