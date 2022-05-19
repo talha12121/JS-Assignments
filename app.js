f47407009ad956ace264f72ebcf8e761104d9772
Talha Khan <tk411635@gmail.com>
2022-05-20 03:27:43 +0500

    chapter 5 completed

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-// Chapter no 1
+                                   // Chapter no 1
+                                   // Chapter no 1
+                                   // Chapter no 1
+
+
 
 // question no 1
 // alert("Hello Talha");
@@ -17,7 +21,9 @@
 // alert("Hello... I can run JS through my web browser's console");
 
 
-// Chapter no 2
+                                    // Chapter no 2
+                                    // Chapter no 2
+                                    // Chapter no 2
 
 // question no 1
 // var  useranme
@@ -65,7 +71,9 @@
 
 
 
-// Chapter no 3
+                                    // Chapter no 3
+                                    // Chapter no 3
+                                    // Chapter no 3
 
 // question No 1
 
@@ -74,8 +82,8 @@
 
 // question No 2
 
-// var visit = 14
-// alert("You Have Visited The Site " + visit + " Times");
+// var visitor = 14
+// alert("You Have Visited The Site " + visitor + " Times");
 
 // question No 3
 
@@ -90,7 +98,9 @@
 // document.write(visitorsName + " ordered " + quantity + " T-Shirts on " +  productTitle);
 
 
-// Chapter No 4
+                                     // Chapter No 4
+                                     // Chapter No 4
+                                     // Chapter No 4
 
 // question no 1
 
@@ -114,7 +124,173 @@
 
 // question no 3
 
-document.write("<h1>" + "Rules For Naming JS Variables" + "</h1>" );
+// document.write("<h1>" + "Rules For Naming JS Variables" + "</h1>" );
+
+// var num = "number"
+// var sign = " $"
+// var variable = "$my_1stVariable"
+// document.write("Variables names can only contain " + " " + num  + sign + " and _. for Example :" + variable );
+
+// var sense = "sensitve"
+// document.write("Variables names are case" + " " + sense);
+
+// var key = "keywords"
+// document.write("Variables names should not be JS" + " " + key);
+
+
+                                     // Chapter no 5
+                                     // Chapter no 5
+                                     // Chapter no 5
+
+// question no 1
+//  var a = 2
+//  var b = 3
+//  var c = a+b
+//  document.write("sum of " + a + " and " + b + " is " + c );
+
+
+// question no 2
+//  var a = 2
+//  var b = 3
+//  var c = a-b
+//  document.write("Subtraction of " + a + " and " + b + " is " + c +"<br>" );
+
+//  var a = 2
+//  var b = 3
+//  var c = a*b
+//  document.write("Multiplication of " + a + " and " + b + " is " + c +"<br>" );
+//  var a = 2
+//  var b = 3
+//  var c = a/b
+//  document.write("Division of " + a + " and " + b + " is " + c +"<br>" );
+//  var a = 2
+//  var b = 3
+//  var c = a%b
+//  document.write("Modulus of " + a + " and " + b + " is " + c +"<br>");
+
+// question no 3
+
+// var a;
+// document.write("Value after variable declaration is" + " " + a + "<br>");
+// var a = 5
+// document.write("Initial value :" + " " + a + "<br>");
+// var increament = ++a
+// document.write(" value after increament is :" + " " + increament + "<br>");
+// var a = a +7
+// document.write(" value after addition is :" + " " + a + "<br>");
+
+// var decrement = --a
+// document.write(" value after decreament is :" + " " + decrement + "<br>");
+// var division = a/3
+// document.write("The remainder is:" + " " + division  + "<br>");
+
+// question no 4
+// var price = 600
+// var total = price * 5
+// document.write("Total cost to buy 5 ticket to a movie is" + " " + total + " PKR" );
+
+// question no 5
+// var num = +prompt("Enter number for Table")
+// for (var i=1 ; i <= 10 ; i++){
+    // document.write(num +"x"+ i + "=" + i*num + "<br>" );
+    // }
+    
+ // question no 6
+//  var celsius = 25
+//  var result = (celsius*9/5)+32
+//  document.write(celsius +"C "+ " is " + result+"F" +"<br>");
+ 
+//  var fahrenheit = 70
+//  var result = (fahrenheit-32)*5/9
+//  document.write(fahrenheit +"F "+ " is " + result+"C" +"<br>");
+
+// question no 7
+var shopName = "Shopping Cart"
+// document.write("<h1>" + shopName + "</h1>" );
+
+
+//  var item1 = 650
+//  var item2 = 100
+//  var quantity1 = 3
+//  var quantity2 = 7
+//  var charge = 100
+//  var total = (item1*quantity1) + (item2*quantity2) + charge
+
+//  document.write("Price of item 1 is" + " " + item1 + "<br>");
+//  document.write("Quantity of item 1 is" + " " + quantity1 + "<br>");
+//  document.write("Price of item 2 is" + " " + item2 + "<br>");
+//  document.write("Quantity of item 2 is" + " " + quantity2 + "<br>");
+//  document.write("Shipping Charges is" + " " + charge + "<br>");
+//  document.write( "<br>");
+//  document.write("Total cost of your order is " + " " + total + "<br>");
+
+// question no 8
+// var card = "MarkSheet"
+// var totalMarks = 980
+// var marksObtained = 804
+// var percent = (marksObtained/totalMarks)*100
+// document.write("<h1>" + card + "</h1>" );
+// document.write("Total Marks=" + " " + totalMarks +"<br>")
+// document.write("Marks Obtained=" + " " + marksObtained +"<br>")
+// document.write("Percentage=" + " " + percent + "%")
+
+// question no 9
+
+//  var heading = "CURRENCY IN PKR"
+//  var dollar$ = 10
+//  var riyal = 25
+//  var total = (104.80*dollar$)+(28*riyal) 
+//  document.write("<h1>" + heading + "</h1>" );
+//  document.write("Total Currency In PKR is" + " " +total + "<br>" );
+
+// question no 10
+
+// var variable = 50
+// var result = (variable+5)*10/2
+// document.write("The Result is" + " " + result);
+
+// question no 11
+
+// var calc = "The Age Calculator"
+// var currentYear = 2022
+// var birthYear = 2002
+// var age = currentYear - birthYear
+//  document.write("<h1>" + calc + "</h1>" );
+//  document.write("Current Year is" + " " + currentYear + "<br>");
+//  document.write("Birth Year is" + " " + birthYear + "<br>");
+// document.write("Your Age is" + " " + age + "<br>");
+
+
+// question no 12
+// var geoMetrizer = "The Geometrizer"
+// var radius = 25
+// var area = 3.14*radius**2
+// var circumference = 2*3.14*radius
+//   document.write("<h1>" + geoMetrizer + "</h1>" );
+// document.write("Radius of a Circle is" + " " + radius + "<br>");
+// document.write("The Cirumference is" + " " + circumference + "<br>");
+// document.write("The Area is" + " " + area + "<br>");
+
+
+//  question no 13
+
+// var calc = "The Lifetime Supply Calculator:"
+// var favouriteSnack = "Chocolate"
+// var currentAge = 19
+// var maximumAge = 90
+// var amountOfSnack = 5
+// var result = (maximumAge-currentAge)*amountOfSnack
+
+// document.write("<h1>" + calc + "</h1>" );
+// document.write("Favourite Snack is" + " " + favouriteSnack + "<br>");
+// document.write("Current Age is" + " " + currentAge + "<br>");
+// document.write("Estimated Maximum Age is" + " " + maximumAge + "<br>");
+// document.write("Amount of Sanck Per Day is" + " " + amountOfSnack + "<br>");
+// document.write("You Will Need"+" " + result + " " + favouriteSnack + " " + "To Last You The Ripe Old Age Of" + " " + maximumAge + "<br>");
+
+
+
+
 
 
 
