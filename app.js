9efc21f82df3f3dd3bf81201623c059f85924dd8
Talha Khan <tk411635@gmail.com>
2022-07-19 19:21:13 +0500

    chapter no 34-38 question no 1-6 completed

--- app.js
+++ app.js
@@ -1322,3 +1322,100 @@
 // document.write("Net Amounts Payable(within due date):"+" "+netAmount +"<br>")
 // document.write("Late Payment Surcharge:"+" "+latePayment +"<br>")
 // document.write("Gross Amount PAyable(after Due Date):"+" "+grossAmount)
+
+
+
+            // CHAPTER  NO 35-38
+            // CHAPTER  NO 35-38
+            // CHAPTER  NO 35-38
+
+// question no 1
+
+// function date(){
+//     var currentDate = new Date()
+//     console.log(currentDate)
+// }
+// date()
+
+// question no 2
+
+// function name(firstName , LastNAme){
+//     var fName = prompt("Enter First Name")
+//     var LName = prompt("Enter Last Name")
+//     var fullName = fName +" "+ LName
+//     alert("Hello"+" "+fullName)
+// }
+// name()
+
+// question no 3
+
+// function num(num1 , num2){
+//      a = +prompt("Enter num1")
+//      b = +prompt("Enter num2")
+//      return a+b
+// }
+// console.log(num())
+
+// questio no 4
+
+// function calc(num1,num2,opt){
+//     num1 = +prompt("Enter Num1")
+//     num2 = +prompt("Enter Num2")
+//     opt = prompt("Enter opt +,-,*,/")
+
+//     if(opt==="+"){
+//         return num1+num2
+//     }
+//     else if(opt==="-"){
+//         return num1-num2
+//     }
+//     else if(opt==="*"){
+//         return num1*num2
+//     }
+//     else if(opt==="/"){
+//         return num1/num2
+//     }else{
+//         return "operator not found"
+//     }
+// }
+
+// alert(calc())
+
+
+// question no 5
+
+// function arg(){
+//     num = +prompt("Enter something")
+//     return num*num
+// }
+// alert(arg())
+
+//             OR
+// function arg(num){
+//    return num*num
+// }
+//  alert(arg(4))
+
+// question no 6
+
+// function factorialize(num) {
+//     if (num < 0) 
+//           return -1;
+    // else if (num == 0) 
+    //     return 1;
+//     else {
+//         return (num * factorialize(num - 1));
+//     }
+//   }
+//   console.log(factorialize(5))
+
+//              OR
+
+// function factorial(n){
+// var h = 1
+// for(var i = 1; i <= n; i++){
+//      h = h*i
+// }
+// return h
+// }
+// alert(factorial(5))
