ff5ea5239db8a534725e7897d45d99ef2b41e5bb
Talha Khan <tk411635@gmail.com>
2022-08-05 22:28:36 +0500

    chapter 35 to 38 completed

--- app.js
+++ app.js
@@ -1419,3 +1419,112 @@
 // return h
 // }
 // alert(factorial(5))
+
+//                OR
+
+// question no 7
+
+// function num(start,end){
+//     var start = +prompt("Enter First Number")
+//     var end = +prompt("Enter last Number")
+//     for(var i=start;i<=end;i++){
+//         console.log(i)
+//     }
+// }
+// num()
+
+// question no 8
+
+// function hyp(base,perp){
+//     var base = +prompt("Enter Base")
+//     var perp = +prompt("Enter Perpendicular")
+//     return Math.sqrt((base*base)+(perp*perp))
+
+// }
+
+// document.write("Hypotenuse ="+ hyp())
+
+// question no 9
+
+// function triangle(a,b){
+//     return a*b
+// }
+// var b = 4
+// console.log(triangle(2,b))
+
+// question no 10
+
+// function palindrome(){
+// var word=prompt("Enter word")
+// var flag="";
+// for(var i=word.length-1;i>=0;i--){
+//     flag+=word[i]
+// }
+// if(flag === word){
+//     return "yes"
+// }
+// else{
+//     return "no"
+// }
+// }
+// console.log(palindrome())
+
+// question no 11
+
+// function upper(){
+//     str = prompt("Enter any Sentences")
+//     str = str.split(' ')
+//     for(var i=0;i<str.length;i++){
+//         str[i]=str[i].charAt(0).toUpperCase() + str[i].slice(1) + " "
+//         console.log(str[i])
+//     }
+
+// }
+// upper()
+
+// question no 12
+
+// function findLongestWord(str) {
+
+// var strSplit = str.split(' ');
+
+// var longestWord = 0;
+
+//     for (var i = 0; i < strSplit.length; i++) {
+//         if (strSplit[i].length > longestWord) {
+//             longestWord = strSplit[i].length;
+//         }
+//     }
+
+//     return longestWord;
+// }
+
+// alert("Longest Word Has " + findLongestWord("The quick brown fox jumped over the lazydog") + "Letters");
+
+// question no 13
+
+
+// function identifier(string,letter){
+//         var c = 0
+//            for(var i = 0; i < string.length; i++){
+//                    if(string[i] === letter){
+//                         c = c + 1
+//                    }
+//            }
+//            return c
+// }
+// var s = prompt("Enter string");
+// var p = prompt("Enter word which is to be count")
+// console.log(identifier(s,p))
+
+
+// question no 14
+
+// function calcCircumference(radius){
+//     return 2*Math.PI*radius
+// }
+// function calcArea(rad){
+//     return Math.round((Math.PI)*rad*rad)
+// }
+// console.log(calcCircumference(12))
+// console.log(calcArea(2))
